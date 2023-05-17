f2292666e001875ab5d3b1df74c9beccad136a6e
Jose Storani <jdstorani91@gmail.com>
2023-05-17 15:56:39 -0300

    correcion centrado de fila

--- src/public/js/userCart.js
+++ src/public/js/userCart.js
@@ -1,20 +1,18 @@
 const deleteProductButton = document.querySelectorAll(".deleteProductButton");
 
-deleteProductButton.forEach((button)=>{
-    const productId = button.id
-button.addEventListener("click",async ()=>{
-    
-   await fetch(`/api/carts/${cartId}/product/${productId}`,{
-        method:"DELETE"
-    })
-    location.reload()
-})
-})
+deleteProductButton.forEach((button) => {
+	const productId = button.id;
+	button.addEventListener("click", async () => {
+		await fetch(`/api/carts/${cartId}/product/${productId}`, {
+			method: "DELETE",
+		});
+		location.reload();
+	});
+});
 
-document.addEventListener("DOMContentLoaded",()=>{
-    const total = document.getElementById('totalValue');
-if (total) {
-  total.textContent = totalValue.toFixed(2);
-}
-})
-
+document.addEventListener("DOMContentLoaded", () => {
+	const total = document.getElementById("totalValue");
+	if (total) {
+		total.textContent = totalValue.toFixed(2);
+	}
+});
